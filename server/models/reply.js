db76db0e63b69b655754b5408d4f92d3bbfb7239
hogyun0126 <rkdghrbs1004@gmail.com>
2022-02-09 14:41:16 +0900

    feat: sequelize

---
+++ server/models/reply.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class reply extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  reply.init({
+    body: DataTypes.STRING,
+    userId: DataTypes.STRING,
+    boardId: DataTypes.STRING,
+    createdAt: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'reply',
+  });
+  return reply;
+};
