484e458862bab0e19e298cb9e6f2c04e9e71ed8d
KimJaewon3 <poilkj1po@gmail.com>
2022-02-27 18:36:28 +0900

    feat: s3 image sever deploy & add function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ import { locationDummy } from './dummyData/location';
 import { RootState } from './modules';
 import { rBoardLts, qBoardLts } from './modules/posts';
 import { locationLts } from './modules/location';
-import PostView from './pages/postView';
+import Test from './pages/test';
 
 function App() {
   const state = useSelector((state: RootState) => state.postsReducer);
@@ -47,6 +47,7 @@ function App() {
             <Route path='/qBoard' element={<QBoard />} />
             <Route path='/rBoard' element={<RBoard />} />
             <Route path='/postEditor' element={<PostEditor />} />
+            <Route path='/test' element={<Test />} />
           </Routes>
         </section>
       </div>
